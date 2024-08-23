af942abebe8231222a299e3e37ef9c71e0582826
kzsharma <kzsharma@daxsws.com>
2024-08-23 16:14:34 +0530

    test

--- src/components/signUp/SignUp.jsx
+++ src/components/signUp/SignUp.jsx
@@ -203,7 +203,7 @@ function SignUp() {
                             </div>
                             <button
                                 onClick={handleSubmit}
-                                className='signup-button'>Sign Up
+                                className='signup-button'>Sign Ups
                             </button>
                         </div>
                     </form>
