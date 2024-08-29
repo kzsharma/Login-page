d3a1e07df4c98d45c31ffd733c0efeaf4b6e475f
kzsharma <kzsharma@daxsws.com>
2024-08-30 02:20:09 +0530

    final changes

---
+++ src/hooks/usevalidate.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function usevalidate(user) {
+  const error = {}
+  const emailPattern = /\S+@\S+\.\S+/;//  return <"ivalid"> {data.error}
+  const passwrodPattern = /^\d+$/;
+
+    if(!emailPattern.test(user.email)){
+      error.email ="invalid Email"
+    }
+    if (user.password.length < 8){
+      error.password ="Invalid password"
+    }
+
+    // console.log(error)
+    return error
+}
+
+
+
+export default usevalidate
